5b22f812a819f6c54919d5b49582624ae6f5f9b7
cereceda1991 <cereceda1991@gmail.com>
2023-04-22 00:00:36 -0500

    Actualización

--- src/components/ResponsiveHome.tsx
+++ src/components/ResponsiveHome.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import Link from 'next/link';
-import CourseCard from './CourseCard';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick-theme.css';
-import 'slick-carousel/slick/slick.css';
-import styles from '@/styles/ResponsiveHome.module.css';
-import Verbify from '../assets/Verbify.png';
-import ManGettingFolder from '../assets/ManGettingFolder.png';
-import ManGettingDegreeOnline1 from '../assets/ManGettingDegreeOnline1.png';
-import BenefitSection from './BenefitSection';
+import React from "react";
+import Link from "next/link";
+import CourseCard from "./CourseCard";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import styles from "@/styles/ResponsiveHome.module.css";
+import Verbify from "../assets/Verbify.png";
+import ManGettingFolder from "../assets/ManGettingFolder.png";
+import ManGettingDegreeOnline1 from "../assets/ManGettingDegreeOnline1.png";
+import BenefitSection from "./BenefitSection";
 
 const courses = [
   {
+    id: 1,
     title: "Nivel básico",
     image: ManGettingFolder.src,
     description:
@@ -19,6 +20,7 @@ const courses = [
   },
 
   {
+    id: 2,
     title: "Nivel intermedio",
     image: ManGettingFolder.src,
     description:
@@ -26,6 +28,7 @@ const courses = [
   },
 
   {
+    id: 3,
     title: "Nivel avanzado",
     image: ManGettingFolder.src,
     description:
@@ -33,6 +36,7 @@ const courses = [
   },
 
   {
+    id: 4,
     title: "Nivel negocios",
     image: ManGettingFolder.src,
     description:
@@ -47,24 +51,26 @@ function ResponsiveHome() {
         <img className={styles.img__1} src={Verbify.src} />
         <div>
           <div className={styles.login}>
-            <Link href="/login">
-              Iniciar Sesión
-            </Link>
+            <Link href="/login">Iniciar Sesión</Link>
           </div>
           <button>
-            <Link href="/register">
-              Registrarme
-            </Link>
+            <Link href="/register">Registrarme</Link>
           </button>
         </div>
         <img className={styles.img__2} src={ManGettingDegreeOnline1.src} />
         <h1>Domina el inglés: la clave para el éxito profesional.</h1>
-        <p>Libera tu potencial con nuestra plataforma de aprendizaje de inglés y lleva tu carrera al siguiente nivel.</p>
+        <p>
+          Libera tu potencial con nuestra plataforma de aprendizaje de inglés y
+          lleva tu carrera al siguiente nivel.
+        </p>
       </section>
       <section className={styles.mainsection}>
         <div className={styles.titlescontainer}>
           <h2 className={styles.maintitle}>Cursos de inglés</h2>
-          <p className={styles.subtitle}> Todos los niveles para que lleves tu ingles al siguiente nivel </p>
+          <p className={styles.subtitle}>
+            {" "}
+            Todos los niveles para que lleves tu ingles al siguiente nivel{" "}
+          </p>
         </div>
       </section>
       <div className={styles.slick}>
@@ -74,7 +80,7 @@ function ResponsiveHome() {
               return (
                 <div key={index} className={styles.carousel}>
                   <CourseCard
-                    key={elem.title}
+                    key={elem.id}
                     title={elem.title}
                     description={elem.description}
                     image={elem.image}
@@ -122,15 +128,20 @@ function ResponsiveHome() {
       </div>
       <div className={styles.container__NewsletterSection}>
         <section className={styles.logo__newsletter}>
-          <img src='https://i.ibb.co/Ct71D8d/Vector.png' alt='vector' />
+          <img src="https://i.ibb.co/Ct71D8d/Vector.png" alt="vector" />
         </section>
         <section className={styles.contenido__newsletter}>
           <h1>Recibe nuestras noticias</h1>
-          <p> Descubre nuevas formas de aprender y mejorar con nuestro
-            newsletter lleno de contenido exclusivo y consejos prácticos.</p>
+          <p>
+            {" "}
+            Descubre nuevas formas de aprender y mejorar con nuestro newsletter
+            lleno de contenido exclusivo y consejos prácticos.
+          </p>
           <form>
-            <input placeholder='Tu correo electrónico' />
-            <button><img src='https://i.ibb.co/z8rdpLs/send-2.png' /></button>
+            <input placeholder="Tu correo electrónico" />
+            <button>
+              <img src="https://i.ibb.co/z8rdpLs/send-2.png" />
+            </button>
           </form>
         </section>
       </div>
@@ -139,21 +150,27 @@ function ResponsiveHome() {
           <nav>
             <div className={styles.footer__list}>
               <ul>
-                <li><h4>Compañía</h4></li>
+                <li>
+                  <h4>Compañía</h4>
+                </li>
                 <li>Acerca de Nosotros</li>
                 <li>Por Qué Elegirnos</li>
                 <li>Precios</li>
                 <li>Testimonios</li>
               </ul>
               <ul>
-                <li><h4>Recursos</h4></li>
+                <li>
+                  <h4>Recursos</h4>
+                </li>
                 <li>Política de Privacidad</li>
                 <li>Términos y Condiciones</li>
                 <li>Blog</li>
                 <li>Contáctanos</li>
               </ul>
               <ul>
-                <li><h4>Producto</h4></li>
+                <li>
+                  <h4>Producto</h4>
+                </li>
                 <li>Gestión de Proyectos</li>
                 <li>Seguimiento de Tiempo</li>
                 <li>Programación de Tiempo</li>
@@ -165,20 +182,20 @@ function ResponsiveHome() {
               <h1>Verbify</h1>
               <h4>Suscríbete a nuestro boletín</h4>
               <form>
-                <input placeholder='Ingresa tu correo electrónico'></input>
+                <input placeholder="Ingresa tu correo electrónico"></input>
                 <button>Suscribirse</button>
               </form>
             </section>
           </nav>
           <section className={styles.logo_verbify}>
             <hr className={styles.linea} />
-            <img src='https://i.ibb.co/TLQS9cS/Verbify-logo.png' />
+            <img src="https://i.ibb.co/TLQS9cS/Verbify-logo.png" />
             <hr className={styles.linea} />
           </section>
         </div>
       </div>
     </div>
-  )
-};
+  );
+}
 
 export default ResponsiveHome;
